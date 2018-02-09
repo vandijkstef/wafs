0a92ea3801138b929d0f1f5b0b26c2f5ddf31fa8
Stef van Dijk <stef@re-beat.nl>
2018-02-09 12:22:35 +0100

    Merge branch 'codereviews/stef'

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,7 +5,7 @@
 	const settings = {
 		debug: true
 	};
-
+	
 	// Debug helper - Splits (temporary) console.logs from solid logs
 	const debug = {
 		log: function(data) {
@@ -14,20 +14,20 @@
 			}
 		}
 	};
-
+	
 	// App
 	class App {
 		constructor() {
 			this.init();
 		}
-
+		
 		// Called by constructor
 		init() {
 			debug.log('Init App');
 			this.setSections();
 			routes.init();
 		}
-
+		
 		// Fetches the available sections from the DOM
 		setSections() {
 			debug.log('Setting menu');
@@ -36,7 +36,7 @@
 			});
 		}
 	}
-
+	
 	// Section class - Contains information about the section and creates and stores the menu item
 	class Section {
 		constructor(section) {
@@ -44,7 +44,7 @@
 			this.menu = this.addMenu(section);
 			viewer.sections.push(this);
 		}
-
+		
 		// Adds the section into the menu - Called by the constructor
 		addMenu(section) {
 			const nav = document.querySelector('nav ul');
@@ -52,28 +52,28 @@
 			const name = section.querySelector('h1').innerText;
 			const li = document.createElement('li');
 			const a = document.createElement('a');
-
+			
 			a.href = '#' + hash;
 			a.innerText = name;
 			li.appendChild(a);
 			nav.appendChild(li);
-
+			
 			return li;
 		}
-
+		
 		// Hide the section and update the menu item
 		hide() {
 			this.section.classList.add('hidden');
 			this.menu.classList.remove('active');
 		}
-
+		
 		// Show the section and update the menu item
 		show() {
 			this.section.classList.remove('hidden');
 			this.menu.classList.add('active');
 		}
 	}
-
+	
 	// Simply sets up the eventlistener and makes sure the app renders a page
 	const routes = {
 		init() {
@@ -84,14 +84,14 @@
 			viewer.setTo(window.location.hash || 'start');
 		}
 	};
-
+	
 	// Controls the section display on the larger scale
 	const viewer = {
 		sections: [],
 		setTo: function(hash) {
 			hash = hash.replace('#', '');
 			debug.log('Setting to ' + hash);
-
+			
 			this.sections.forEach(function(section) {
 				if (section.section.id !== hash) {
 					section.hide();
@@ -101,7 +101,7 @@
 			});
 		}
 	};
-
+	
 	window.addEventListener('DOMContentLoaded', function() {
 		new App();
 	});
