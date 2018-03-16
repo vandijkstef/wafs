57d115d7bce2f574cb4b4ae7ff11e0fa23c6a1c6
Stef van Dijk <stef@vandijkstef.nl>
2018-03-16 11:38:54 +0100

    Makes localStorage completely optional
    fix #4

--- app/static/js/appDataHelper.js
+++ app/static/js/appDataHelper.js
@@ -4,6 +4,10 @@ import debug from './debug.js';
 
 const appDataHelper = {
 	store: function(appData) {
+		if (!localStorage) {
+			console.warn('Local storage is disabled', 'Store');
+			return;
+		}
 		// Copy the appData into something we can work with without having it reflect in the actual appData
 		let workData = Object.assign({}, appData);
 		workData.git.repos.forEach((repo) => {
@@ -16,6 +20,10 @@ const appDataHelper = {
 	},
 	fetch: function() {
 		let appData = new AppData();
+		if (!localStorage) {
+			console.warn('Local storage is disabled', 'Fetch');
+			return appData;
+		}
 		if (localStorage.getItem('appData')) {
 			const fetchedData = JSON.parse(localStorage.getItem('appData'));
 			fetchedData.git.repos.forEach(function(repo) {
